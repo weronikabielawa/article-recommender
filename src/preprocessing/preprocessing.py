c5771bf6940449851937557c2eb46fd748a8ce55
weronika <bielawa.weronika@gmail.com>
2022-06-01 17:24:45 +0200

    add lemmatization

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import morfeusz2
 #from pystempel import Stemmer
 
 DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../' + 'data/')
@@ -7,22 +8,22 @@ DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../'
 df = pd.read_csv(DATA_FOLDER+'raw_data/ekologia.csv')
 
 
-def remove_puncuation(df):
+def remove_puncuation(df, col_name):
     punctuation = ['.', ',']
-    df['page_content'] = df.apply(lambda row: "".join([i for i in row['page_content'] if i not in punctuation]),
+    df[col_name] = df.apply(lambda row: "".join([i for i in row[col_name] if i not in punctuation]),
                                         axis=1)
     return df
 
 
-def remove_stopwords(df):
+def remove_stopwords(df, col_name):
     stop_words = open(DATA_FOLDER + 'helpers/polish_stopwords.txt').read().split("\n")
-    df['page_content'] = df.apply(lambda row: " ".join([i for i in row['page_content'].split(" ") if i not in
+    df[col_name] = df.apply(lambda row: " ".join([i for i in row[col_name].split(" ") if i not in
                                                         stop_words]), axis=1)
     return df
 
 
-def lowercase(df):
-    df['page_content'] = df.apply(lambda row: " ".join([i.lower() for i in row['page_content'].split(" ")]), axis=1)
+def lowercase(df, col_name):
+    df[col_name] = df.apply(lambda row: " ".join([i.lower() for i in row[col_name].split(" ")]), axis=1)
     return df
 
 #def stemming(df):
@@ -36,9 +37,29 @@ def lowercase(df):
 
 #stop_words = open(DATA_FOLDER+'helpers/polish_stopwords.txt').read().split("\n")
 
-df = remove_puncuation(df)
-df = lowercase(df)
-df = remove_stopwords(df)
-#df = stemming(df)
+def lemme(df, col_name):
+
+    def lem(s):
+        result = []
+        morf = morfeusz2.Morfeusz()
+        a = morf.analyse(s)
+        i=0
+        for j in a:
+            if j[0] == i:
+                i += 1
+                result.append(j[2][1])
+        return "".join(s)
+    #morf = morfeusz2.Morfeusz()
+
+    df[col_name] = df.apply(lambda row: lem(row[col_name]), axis=1)
+    return df
+
+df = df.head(5)
+#print(x)
+df = remove_puncuation(df, 'page_content')
+df = lowercase(df, 'page_content')
+df = remove_stopwords(df, 'page_content')
 print(df['page_content'])
+df = lemme(df, 'page_content')
+print(df['page_content'].loc[0])
 #print(df)
