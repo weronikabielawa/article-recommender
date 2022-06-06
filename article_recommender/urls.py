c1e7991a62f7dbfe8a4b8ea828f21cc385683db3
weronika <bielawa.weronika@gmail.com>
2022-06-06 22:09:50 +0200

    django helloworld

--- article_recommender/urls.py
+++ article_recommender/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import app.views
+
 urlpatterns = [
+    path("", app.views.index, name="index"),
+    #path("db/", hello.views.db, name="db"),
     path('admin/', admin.site.urls),
 ]
