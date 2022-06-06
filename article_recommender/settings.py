95284c2978a73708ed9e26d96857eadb34cc068c
weronika <bielawa.weronika@gmail.com>
2022-06-06 18:53:07 +0200

    Automatic configuration with django-heroku

--- article_recommender/settings.py
+++ article_recommender/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +28,8 @@ SECRET_KEY = 'django-insecure-^2qd*j)b)psfgl(t4fz3z96$o1k@2zxkebmqhre*y7@ms5-w#e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
+
 
 
 # Application definition
