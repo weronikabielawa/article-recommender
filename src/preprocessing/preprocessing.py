2522f2c6e2c2d9b5707deed2a96928d6d42e801e
weronika <bielawa.weronika@gmail.com>
2022-06-08 11:11:18 +0200

    fix lemmetizer

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import morfeusz2
+import re
 #from pystempel import Stemmer
 
 DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../' + 'data/')
@@ -8,22 +9,39 @@ DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../'
 df = pd.read_csv(DATA_FOLDER+'raw_data/ekologia.csv')
 
 
-def remove_puncuation(df, col_name):
-    punctuation = ['.', ',']
+def cut_ending(df, end_str, col_name='page_content'):
+    df[col_name] = df.apply(lambda row: row[col_name].split(end_str)[0], axis=1)
+    return df
+
+
+def remove_http(df, col_name='page_content'):
+    df[col_name] = df.apply(lambda row: re.sub(r"http\S+", "", row[col_name]), axis=1)
+    return df
+
+
+def remove_puncuation(df, col_name='page_content'):
+    punctuation = '.,();-„”-:–?!"&'
     df[col_name] = df.apply(lambda row: "".join([i for i in row[col_name] if i not in punctuation]),
                                         axis=1)
     return df
 
 
-def remove_stopwords(df, col_name):
+def remove_stopwords(df, col_name='page_content'):
     stop_words = open(DATA_FOLDER + 'helpers/polish_stopwords.txt').read().split("\n")
     df[col_name] = df.apply(lambda row: " ".join([i for i in row[col_name].split(" ") if i not in
                                                         stop_words]), axis=1)
     return df
 
 
-def lowercase(df, col_name):
+def lowercase(df, col_name='page_content'):
     df[col_name] = df.apply(lambda row: " ".join([i.lower() for i in row[col_name].split(" ")]), axis=1)
+    return df
+
+def remove_tokens(df, col_name='page_content'):
+    tokens = ['\r', '\n', '\xa0']
+    for token in tokens:
+        df[col_name] = df.apply(lambda row: row[col_name].replace(token, ""), axis=1)
+
     return df
 
 #def stemming(df):
@@ -37,7 +55,8 @@ def lowercase(df, col_name):
 
 #stop_words = open(DATA_FOLDER+'helpers/polish_stopwords.txt').read().split("\n")
 
-def lemme(df, col_name):
+
+def lemme(df, col_name='page_content'):
 
     def lem(s):
         result = []
@@ -47,19 +66,33 @@ def lemme(df, col_name):
         for j in a:
             if j[0] == i:
                 i += 1
-                result.append(j[2][1])
-        return "".join(s)
-    #morf = morfeusz2.Morfeusz()
+                result.append(j[2][1].split(':')[0])
+        return " ".join(result)
 
-    df[col_name] = df.apply(lambda row: lem(row[col_name]), axis=1)
+
+    for index, row in df.iterrows():
+        df.loc[index, col_name] = lem(row[col_name])
+
     return df
 
-df = df.head(5)
-#print(x)
-df = remove_puncuation(df, 'page_content')
-df = lowercase(df, 'page_content')
-df = remove_stopwords(df, 'page_content')
-print(df['page_content'])
-df = lemme(df, 'page_content')
-print(df['page_content'].loc[0])
-#print(df)
+
+def preprocess(df, col_name='page_content'):
+    # to remove nan (there was 1 example found)
+    df = df[~df[col_name].isna()]
+
+    #for ekologia
+    df = cut_ending(df, 'Ekologia.')
+    #for interia
+    df = cut_ending(df, 'Korzystanie z portalu oznacza akceptację Regulaminu.')
+    # for zielonewiadomosci.pl
+    df = cut_ending(df, 'Źródło')
+    df = remove_http(df, col_name)
+    df = remove_tokens(df, col_name)
+    df = remove_puncuation(df, col_name)
+    df = lowercase(df, col_name)
+    df = remove_stopwords(df, col_name)
+    #df = lemme(df, col_name)
+
+
+    return df
+
