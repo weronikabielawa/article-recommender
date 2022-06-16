b441aba8c8adae371d9da8998a13191bf5c2ecee
weronika <bielawa.weronika@gmail.com>
2022-06-16 19:40:51 +0200

    add preprocessing for spacy tok2vec

--- src/models/tok2vec_pretrained.py
+++ src/models/tok2vec_pretrained.py
@@ -3,34 +3,32 @@ import spacy
 import pandas as pd
 import os
 
+from src.preprocessing.preprocessing import preprocess
+
 global DATA_FOLDER
 DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../' + 'data/')
 
 
 def recommend(article):
-    nlp = spacy.load('pl_core_news_md',  disable=[
-    'morphologizer',
-    'parser',
-    'lemmatizer',
-    'tagger',
-    'senter',
-    'attribute_ruler',
-    'ner'
-    ])
-    print("?")
+    nlp = spacy.load('pl_core_news_md',  disable=['morphologizer',
+                                                  'parser',
+                                                  'lemmatizer',
+                                                  'tagger',
+                                                  'senter',
+                                                  'attribute_ruler',
+                                                  'ner'])
 
-    #embed_mat = pd.read_pickle(DATA_FOLDER + 'preprocessed_data/embed_mat.pkl')
-    #embed_mat = embed_mat['page_content'].to_numpy()
+    #preprocessing input
+    article_df = preprocess(pd.DataFrame({'page_content': [article]}))
+    article_preprocessed = article_df.loc[0, 'page_content']
 
     df = pd.read_csv(DATA_FOLDER + 'preprocessed_data/preprocessed_data.csv')
     df = df[~df['page_content'].isna()]
     df['page_content'] = df['page_content'].apply(lambda x: nlp(x))
     embed_mat = df['page_content'].values
 
-    query_embed = nlp(article)
-    print("here")
+    query_embed = nlp(article_preprocessed)
     mat = np.array([query_embed.similarity(line) for line in embed_mat])
-    print("here2")
     index = np.argmax(mat)
 
     return df.loc[index, 'url']
@@ -42,11 +40,8 @@ def save():
     df = pd.read_csv(DATA_FOLDER + 'preprocessed_data/preprocessed_data.csv')
     df = df[~df['page_content'].isna()]
 
-    print("here")
     df['page_content'] = df['page_content'].apply(lambda x: nlp(x))
-    print("here2")
     embed_mat = df['page_content'].values
-    print("here3")
 
     df = pd.DataFrame({"page_content": embed_mat})
     df.to_pickle(DATA_FOLDER + 'preprocessed_data/embed_mat.pkl')
