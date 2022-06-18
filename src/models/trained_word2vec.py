de5fe8f93d832bcd9e9f320577596432705e6459
weronika <bielawa.weronika@gmail.com>
2022-06-18 14:05:03 +0200

    trained doc2vec for all data

--- src/models/trained_word2vec.py
+++ src/models/trained_word2vec.py
@@ -9,10 +9,11 @@ def recommend_doc2vec(article):
     DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../' + 'data/')
     df = pd.read_csv(DATA_FOLDER+'preprocessed_data/preprocessed_data.csv')
 
-    df['page_content'] = df['page_content'].str.split(" ")
-    documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df['page_content'])]
+    #df['page_content'] = df['page_content'].str.split(" ")
+    #documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df['page_content'])]
 
-    model = Doc2Vec(documents, vector_size=5, window=2, min_count=1, workers=4)
+    #model = Doc2Vec(documents, vector_size=5, window=2, min_count=1, workers=4)
+    model = Doc2Vec.load(DATA_FOLDER+'preprocessed_data/trained_model')
 
     # preprocessing input
     article_df = preprocess(pd.DataFrame({'page_content': [article]}))
@@ -22,3 +23,20 @@ def recommend_doc2vec(article):
     index = model.dv.most_similar(vector)[0][0]
 
     return df.loc[index, 'url']
+
+
+def train():
+    DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../' + 'data/')
+    df = pd.read_csv(DATA_FOLDER+'preprocessed_data/preprocessed_data.csv')
+    df = df[~df['page_content'].isna()]
+    df['page_content'] = df['page_content'].str.split(" ")
+    documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df['page_content'])]
+
+
+    model = Doc2Vec(documents, vector_size=500, window=2, min_count=1, workers=4)
+
+    model.save(DATA_FOLDER+'preprocessed_data/trained_model')
+
+
+if __name__=='__main__':
+    train()
