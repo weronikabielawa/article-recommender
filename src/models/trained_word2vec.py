2041991f2036a0c63ed15a0f1f826ab6ddf84eaf
weronika <bielawa.weronika@gmail.com>
2022-08-22 00:01:25 +0200

    refactor

--- src/models/trained_word2vec.py
+++ src/models/trained_word2vec.py
@@ -7,14 +7,8 @@ global DATA_FOLDER
 DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../' + 'data/')
 
 def recommend_doc2vec(article_preprocessed):
-    #df['page_content'] = df['page_content'].str.split(" ")
-    #documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df['page_content'])]
-
-    #model = Doc2Vec(documents, vector_size=5, window=2, min_count=1, workers=4)
     model = Doc2Vec.load(DATA_FOLDER+'preprocessed_data/trained_model')
-
     article_preprocessed = article_preprocessed.loc[0, 'page_content'].split(" ")
-
     vector = model.infer_vector(article_preprocessed)
     index = model.dv.most_similar(vector)[0][0]
 
@@ -27,10 +21,7 @@ def train():
     df = df[~df['page_content'].isna()]
     df['page_content'] = df['page_content'].str.split(" ")
     documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(df['page_content'])]
-
-
     model = Doc2Vec(documents, vector_size=10, window=2, min_count=1, workers=4)
-
     model.save(DATA_FOLDER+'preprocessed_data/trained_model')
 
 
